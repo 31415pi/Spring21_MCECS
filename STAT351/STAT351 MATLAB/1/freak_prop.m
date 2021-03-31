cf63987a71fa6b8e09666f52b8178696738a5cde
wekster <31289154+31415pi@users.noreply.github.com>
2021-03-30 18:45:25 -0700

    Update freak_prop.m

--- STAT351/STAT351 MATLAB/1/freak_prop.m
+++ STAT351/STAT351 MATLAB/1/freak_prop.m
@@ -1,6 +1,5 @@
-function freak_prop(inputArg1)
-%FREAK_PROP Create Frequemcy and Proportion Populations on inputn number of
-%samples.
+function freak_prop(inputArg1,bin_div)
+%FREAK_PROP Create Frequemcy and Proportion Populations on input1 number of samples displayed across 0.1/input2 num bins
 % Generate and plot some random sample; Normal distribution; Create a histogram with Frequency;% Create a histogram with Proportion
 %  MKULTRA https://github.com/31415pi/Spring21_MCECS/
 
@@ -15,13 +14,13 @@ ylabel('Observed output value');
  xlabel('Button push');
 %
 figure(4);
-histogram(x,'BinWidth',0.1);
+histogram(x,'BinWidth',0.1/bin_div);
  xlim([0 1]);
 ylabel('Frequency (Number of values)');
  xlabel('x value');
 %
 figure(5);
- histogram(x,'Normalization', 'probability','BinWidth',0.1);
+ histogram(x,'Normalization', 'probability','BinWidth',0.1/bin_div);
  xlim([0 1]);
 ylabel('Fraction (Proportion) of values');
  xlabel('x value');
