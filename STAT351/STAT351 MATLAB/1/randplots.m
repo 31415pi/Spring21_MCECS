bfa938dc6ad984cdeca782e8876c5c9890d78cb7
wekster <31289154+31415pi@users.noreply.github.com>
2021-03-30 18:26:08 -0700

    Update randplots.m

--- STAT351/STAT351 MATLAB/1/randplots.m
+++ STAT351/STAT351 MATLAB/1/randplots.m
@@ -1,6 +1,6 @@
 function randplots(inputArg1,inputArg2)
 %RANDPLOTS - creates two figures plotting output vals and histogram of input1 number of random points put in input2 number of bins.
-%Generate and plot some random sample; 1- Uniform distribution; 2- Create a histogram
+%Generate and plot some random sample; 1- Uniform distribution; 2- Create a histogram ;   MKULTRA https://github.com/31415pi/Spring21_MCECS/
 
 N=inputArg1;
 %1
